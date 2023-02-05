3c5c05e0b1f9f0ed4453e31914d5fb8f65a4ba5d
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-02-05 23:13:56 +0700

    Добавьте файлы проекта.

---
+++ ColoredRectangle/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ColoredRectangle.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        string rectangle0 = "LightGray";
+        string rectangle1 = "LightGray";
+        string rectangle2 = "LightGray";
+        string rectangle3 = "LightGray";
+        string rectangle4 = "LightGray";
+        string rectangle5 = "LightGray";
+        string rectangle6 = "LightGray";
+        string rectangle7 = "LightGray";
+        string rectangle8 = "LightGray";
+        string rectangle9 = "LightGray";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
