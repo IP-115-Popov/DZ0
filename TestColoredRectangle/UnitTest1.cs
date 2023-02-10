f610a15d834669a74086be17d8dc9e56ac9f3e77
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-02-10 21:07:30 +0700

    Add test

---
+++ TestColoredRectangle/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestColoredRectangle
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
