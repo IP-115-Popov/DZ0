b63c9f9d1692612dc0d611911321a05fcc01b545
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-02-12 18:01:37 +0700

    Add xUnit test and remuve old test

--- TestColoredRectangle/UnitTest1.cs
+++ TestColoredRectangle/UnitTest1.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+using Avalonia.VisualTree;
 
 namespace TestColoredRectangle
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public async void Test1()
         {
+            var app = AvaloniaApp.GetApp();
+            var mainWindow = AvaloniaApp.GetMainWindow();
+            Color c = Colors.Red;
+
+            await Task.Delay(100);
+
+            var button = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "ButtonRed");
+            var rectangle = mainWindow.GetVisualDescendants().OfType<Rectangle>().First();
+
+            button.Command.Execute(button.CommandParameter);
+
+            await Task.Delay(50);
+
+            var rectColor = (Color?)(rectangle.Fill as SolidColorBrush).Color;
+
+            Assert.True(rectColor.Equals(c));
         }
     }
 }
