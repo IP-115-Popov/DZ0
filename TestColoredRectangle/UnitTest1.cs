8bd9345256631a6d09e23761789296160ffc197f
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-02-12 18:07:15 +0700

    Add commends

--- TestColoredRectangle/UnitTest1.cs
+++ TestColoredRectangle/UnitTest1.cs
@@ -10,19 +10,24 @@ namespace TestColoredRectangle
         [Fact]
         public async void Test1()
         {
+            //получаем ссылку на приложение
             var app = AvaloniaApp.GetApp();
+            //поллучаем ссылку на окно, приложения
             var mainWindow = AvaloniaApp.GetMainWindow();
+            //ожидаемое значение цвета
             Color c = Colors.Red;
 
             await Task.Delay(100);
 
+            //получаем ссылку на первую кнопку в окне с подходящим именем
             var button = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "ButtonRed");
+            //получаем ссылку на первый попавшийся премоугольник
             var rectangle = mainWindow.GetVisualDescendants().OfType<Rectangle>().First();
-
+            //нажатие на кнопку
             button.Command.Execute(button.CommandParameter);
 
             await Task.Delay(50);
-
+            //получение цвета прямоугольника
             var rectColor = (Color?)(rectangle.Fill as SolidColorBrush).Color;
 
             Assert.True(rectColor.Equals(c));
